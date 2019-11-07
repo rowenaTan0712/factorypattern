727618967328aee37bf1faf41d37c30d5c246431
Ma. Rowena Tan <rowena.tan0712@gmail.com>
2019-11-07 18:36:37 +0800

    spring boot app

---
+++ src/test/java/com/java/factorypattern/factorypattern/FactorypatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.factorypattern.factorypattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactorypatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
